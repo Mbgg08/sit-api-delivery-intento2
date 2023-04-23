b8b9bde537dd978e61c6da6703b29bc026c0ce4c
Mbgg08 <mgutierrezg11@upao.edu.pe>
2023-04-23 18:38:16 -0500

    feat: Crear clase Food con los atributos name, decription y price

--- api-delivery/src/main/java/upao/edu/pe/apidelivery/entities/Food.java
+++ api-delivery/src/main/java/upao/edu/pe/apidelivery/entities/Food.java
@@ -1,4 +1,37 @@
 package upao.edu.pe.apidelivery.entities;
 
 public class Food {
+    private String name;
+    private String descritpion;
+    private int price;
+
+    public Food(String name, String descritpion, int price){
+        this.name = name;
+        this.descritpion = descritpion;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescritpion() {
+        return descritpion;
+    }
+
+    public void setDescritpion(String descritpion) {
+        this.descritpion = descritpion;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
